645c960ee9e920352b799cdf2fa59e7eb79bc28e
Omar Meharab <omarmeharab81@gmail.com>
2021-02-07 01:29:09 +0600

    none

---
+++ undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+        console.log(num1, num2);
+}
+
+const lover = {name:"smart dude",  phone:"123414"};
+console.log(lover.gf);
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2, 5, 8];
+console.log(ages[11]);
